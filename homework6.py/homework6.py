8edb1879c9935812809ea12bb2c0a0bf4bc558df
moxamad <R8AGn5VhFSy7Rz@yandex.ru>
2024-07-01 15:55:43 +0300

    Add files via upload

---
+++ homework6.py/homework6.py
@@ -0,0 +1,14 @@
+my_dict={'s': 78,'x':79,'z':80}
+my_dict['d']=81
+print(my_dict['s'])
+print(my_dict['d'])
+my_dict.update({'a':82,'c':83})
+print(my_dict)
+del my_dict['s']
+print(my_dict)
+my_set={1,2,1,2,3,4,3,4,('1','2')}
+print(my_set)
+my_set.add(5),my_set.add(6)
+print(my_set)
+my_set.discard(1)
+print(my_set)
